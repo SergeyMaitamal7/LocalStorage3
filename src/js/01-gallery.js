38ae3a4c1c9f28d9d338e7daa3ae12828d0df716
SergeyMaitamal7 <96728835+SergeyMaitamal7@users.noreply.github.com>
2024-01-31 15:19:53 +0200

    finish

---
+++ src/js/01-gallery.js
@@ -0,0 +1,38 @@
+
+import { galleryItems } from './gallery-items.js';
+import simpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const gallery = document.querySelector('.gallery');
+
+gallery.addEventListener('click', onClick);
+document.addEventListener('keydown', onCloseModal);
+
+const galleryItemList = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+ </li>`;
+  })
+  .join('');
+gallery.insertAdjacentHTML('beforeend', galleryItemList);
+
+function onClick(e) {
+  e.preventDefault();
+}
+
+ function onCloseModal(e) {
+  if(lightbox.isOpen && e.code === 'ESCAPE') {
+    lightbox.isOpen='false' 
+   }
+ }
+
+let lightboxModal = new SimpleLightbox('.gallery a', {
+  captionPosition: 'bottom',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
